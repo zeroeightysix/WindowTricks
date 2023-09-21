4ac3ba1d69c8f2454aed847863e6e580ad8fda0a
Ridan Vandenbergh <ridanvandenbergh@gmail.com>
2023-09-21 19:53:24 +0200

    Initial commit

---
+++ TransWindows/Plugin.cs
@@ -0,0 +1,75 @@
+﻿using Dalamud.Game.Command;
+using Dalamud.Interface.Windowing;
+using Dalamud.IoC;
+using Dalamud.Plugin;
+using TransWindows.Windows;
+
+namespace TransWindows
+{
+    public sealed class Plugin : IDalamudPlugin
+    {
+        public string Name => "TransWindows";
+        private const string CommandName = "/twindows";
+
+        private DalamudPluginInterface PluginInterface { get; init; }
+        private CommandManager CommandManager { get; init; }
+        public Configuration Configuration { get; init; }
+        public WindowSystem WindowSystem = new("TransWindows");
+
+        private ConfigWindow ConfigWindow { get; init; }
+        private MainWindow MainWindow { get; init; }
+
+        public Plugin(
+            [RequiredVersion("1.0")] DalamudPluginInterface pluginInterface,
+            [RequiredVersion("1.0")] CommandManager commandManager)
+        {
+            this.PluginInterface = pluginInterface;
+            this.CommandManager = commandManager;
+
+            this.Configuration = this.PluginInterface.GetPluginConfig() as Configuration ?? new Configuration();
+            this.Configuration.Initialize(this.PluginInterface);
+
+            // you might normally want to embed resources and load them from the manifest stream
+
+            ConfigWindow = new ConfigWindow(this);
+            MainWindow = new MainWindow(this);
+
+            WindowSystem.AddWindow(ConfigWindow);
+            WindowSystem.AddWindow(MainWindow);
+
+            this.CommandManager.AddHandler(CommandName, new CommandInfo(OnCommand)
+            {
+                HelpMessage = "A useful message to display in /xlhelp"
+            });
+
+            this.PluginInterface.UiBuilder.Draw += DrawUI;
+            this.PluginInterface.UiBuilder.OpenConfigUi += DrawConfigUI;
+        }
+
+        public void Dispose()
+        {
+            this.WindowSystem.RemoveAllWindows();
+
+            ConfigWindow.Dispose();
+            MainWindow.Dispose();
+
+            this.CommandManager.RemoveHandler(CommandName);
+        }
+
+        private void OnCommand(string command, string args)
+        {
+            // in response to the slash command, just display our main ui
+            MainWindow.IsOpen = true;
+        }
+
+        private void DrawUI()
+        {
+            this.WindowSystem.Draw();
+        }
+
+        public void DrawConfigUI()
+        {
+            ConfigWindow.IsOpen = true;
+        }
+    }
+}
