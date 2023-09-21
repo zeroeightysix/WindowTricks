4e4c5bea2d666a98c1d91a1eb8b481e5cd7fa724
Ridan Vandenbergh <ridanvandenbergh@gmail.com>
2023-09-21 23:06:05 +0200

    Implement HiddenParents

--- WindowTricks/NativeUI.cs
+++ WindowTricks/NativeUI.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Dalamud.Logging;
+using Dalamud.Memory;
 using FFXIVClientStructs.FFXIV.Component.GUI;
 using FFXIVClientStructs.Interop;
 
@@ -11,7 +13,13 @@ public static class NativeUI
     // Some sub-windows don't have a `ParentID`. We store the known ones here.
     private static Dictionary<string, string> HiddenParents = new Dictionary<string, string>
     {
-        { "FreeCompanyMember", "FreeCompany" }
+        { "FreeCompanyTopics", "FreeCompany" },
+        { "FreeCompanyMember", "FreeCompany" },
+        { "FreeCompanyRank", "FreeCompany" },
+        { "FreeCompanyRights", "FreeCompany" },
+        { "FreeCompanyAction", "FreeCompany" },
+        { "FreeCompanyActivity", "FreeCompany" },
+        { "FreeCompanyStatus", "FreeCompany" },
     };
 
     public static unsafe void ResetAlphas()
@@ -23,15 +31,23 @@ public static class NativeUI
             (&five->AtkUnitEntries)[index]->SetAlpha(255);
         }
     }
-    
+
     public static unsafe AtkUnitBase* FollowUp(AtkUnitBase* unitBase, Action<Pointer<AtkUnitBase>>? consumer = null)
     {
+        var rapture = AtkStage.GetSingleton()->RaptureAtkUnitManager;
+
         while (true)
         {
             consumer?.Invoke(unitBase);
             var parentId = unitBase->ParentID;
-            if (parentId == 0) return unitBase;
-            unitBase = AtkStage.GetSingleton()->RaptureAtkUnitManager->GetAddonById(parentId);
+            if (parentId == 0)
+            {
+                var name = MemoryHelper.ReadStringNullTerminated((IntPtr)unitBase->Name);
+                if (HiddenParents.TryGetValue(name, out var parent))
+                    unitBase = rapture->GetAddonByName(parent);
+                else return unitBase;
+            }
+            else unitBase = rapture->GetAddonById(parentId);
         }
     }
 }
